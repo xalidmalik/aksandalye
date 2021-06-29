9549ac67dbb396ced1b687192e6739209134a049
Xalid Malik <xalidmelikov@gmail.com>
2021-06-29 04:33:51 +0300

    separate category and product page base

---
+++ pages/products/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        Products
+      </main>
+    </div>
+  );
+}
