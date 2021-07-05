b4846cecb55aee0c0927c2c8316e6789dc4303d5
Xalid Malik <xalidmelikov@gmail.com>
2021-07-05 23:31:48 +0300

    add container config to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./sources/**/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "2rem",
+        sm: "1rem",
+        lg: "1rem",
+        xl: "1rem",
+        "2xl": "6rem",
+      },
+      center: true,
+    },
     extend: {},
   },
   variants: {
